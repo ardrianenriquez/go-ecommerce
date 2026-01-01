1c334e5cfabce064fed4ab9119de5f1199f54fa6
Ardrian Enriquez <ard.lois00@live.com>
2026-01-01 18:36:17 +0800

    Added timestamps on user

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,10 +1,12 @@
 package domain
 
+import "time"
+
 type User struct {
 	ID           int64
 	Email        string
 	PasswordHash string
 	Role         string
-	// CreatedAt
-	// UpdatedAt
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
 }
